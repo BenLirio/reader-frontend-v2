86b5a42732c2fc664bb282588fac9ae4d2112af2
Ben Lirio <benjlirio@gmail.com>
2020-07-05 21:50:07 -0400

    base works

--- src/App.js
+++ src/App.js
@@ -2,39 +2,79 @@ import React, { useState } from 'react'
 import classes from './App.module.css'
 import config from './config'
 import axios from 'axios'
+import { ImageAnnotatorClient } from '@google-cloud/vision'
 
 // firebase
 import firebaseKey from './firebase-key'
-
 import firebase from 'firebase'
 
+import { base64 } from 'rfc4648'
+// const json = {
+//   inputConfig: {
+//     INPUT_CONFIG
+//   },
+//   features: [
+//     FEATURE
+//   ],
+//   imageContext: {
+//     IMAGE CONTEXT
+//   },
+//   pages: [
+//     number
+//   ]
+// }
+let start
+let end
+console.log('base64.stringify([1,2,3])', base64.stringify([1, 2, 3]))
 const app = firebase.initializeApp(firebaseKey)
-
-
+console.log('app', app)
+const vision = new ImageAnnotatorClient({ auth: app.options.apiKey, fallback: true })
+console.log('vision', vision)
 const App = () => {
-  const [filename, setFilename] = useState('')
-  const handleFilenameChange = (e) => {
-    const { value } = e.target
-    setFilename(value)
-  }
-  const fileSubmit = async () => {
-    const { data } = await axios({
+  const handleFile = async (e) => {
+    start = new Date()
+    const content = await toBase64(e.target.files[0])
+    console.log(content.replace(/[^.]*,/, ''))
+    const request = {
+      image: {
+        content: content.replace(/[^.]*,/, ''),
+      },
+      features: [
+        {
+          type: 'DOCUMENT_TEXT_DETECTION',
+          model: 'builtin/latest'
+        }
+      ],
+      imageContext: {
+
+      }
+    }
+    axios({
       method: "POST",
-      url: `${config.apiUrl}/analyze`,
-      data: { filename }
+      url: `https://vision.googleapis.com/v1/images:annotate?key=${app.options.apiKey}`,
+      data: {
+        requests: [request]
+      },
+    }).then((res) => {
+      console.log('res', res)
+      end = new Date()
+      console.log(end - start)
     })
+    // vision.textDetection('gs://reader-images-v2/sample.png')
   }
+
   return (
     <>
-      <div className={classes.root}>
-        <h1>Enter file name</h1>
-        <form>
-          <input onChange={handleFilenameChange} value={filename} type="text" placeholder="filename" />
-          <input type="button" value="go" onClick={fileSubmit} />
-        </form>
-      </div>
+      <input type="file" onChange={handleFile}></input>
     </>
   )
 }
 
 export default App
+
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onload = () => resolve(reader.result)
+  reader.onerror = error => reject(error)
+})
